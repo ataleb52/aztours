68bacc3a93a5af608fda5b6a5f459c436e3498bc
Ahmad Taleb <ataleb52@gmail.com>
2015-04-05 07:42:51 -0700

    Intital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aztours::Application.config.secret_key_base = '0448e568c12c021eb340ce367689f9eae38aab243a69079b69ce7d1b70040dd34adac44efb8564eff0033afc6b2a102462140f10b10ded2f0b82998fef1303dc'
